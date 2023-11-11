23f0415499365670e6ca8da9e5edea433b975a28
Greg Fenton <greg.fenton@gmail.com>
2023-11-11 10:30:03 -0500

    remove vite-plugin-dts to try to debug `storybook build` error

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
+// import dts from 'vite-plugin-dts'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
-  plugins: [react(), dts({ include: 'lib' }), libInjectCss()],
+  plugins: [
+    react(),
+    //  dts({ include: 'lib' }),
+    libInjectCss(),
+  ],
   build: {
     // do not copy the contents of the public folder to the dist folder
     copyPublicDir: false,
